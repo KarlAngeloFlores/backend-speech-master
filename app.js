a7910164ba840c7bf00117630e2f29355524beb2
KarlAngeloFlores <floreskarlangelo03@gmail.com>
2025-10-31 19:32:41 +0800

    bug fixes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://192.168.1.9:5173',
   'https://speechmaster.netlify.app',
+  'https://frontend-speech-master.vercel.app'
 ];
 
 const app = express();
